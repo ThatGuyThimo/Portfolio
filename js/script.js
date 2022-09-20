1ed2640b665161d03ebbc792d1e4b7ebca34095e
thimo <thimodehaan@gmail.com>
2022-09-20 16:04:41 +0200

    added arrow and better scroll to

--- js/script.js
+++ js/script.js
@@ -52,3 +52,33 @@
       var skills = new SkillsBar( ".skillbar-bar" );
     });
   })();
+
+  document.getElementById("home").onclick = function() {
+    window.scrollTo({top: 0, behavior: 'smooth'})
+}
+  document.getElementById("aboutMe").onclick = function() {
+    let element = document.getElementById("introduction")
+    element.scrollIntoView({ behavior: 'smooth', block: 'center'});
+}
+  document.getElementById("arrow").onclick = function() {
+    let element = document.getElementById("introduction")
+    element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+let navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+let sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
